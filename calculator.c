1ff345a63d51cc2d6dbb652be9dc114bb29ff167
Joseph Bell <jbell@numerex.com>
2012-03-14 13:30:21 -0500

    Updating calculator to use version info

--- calculator.c
+++ calculator.c
@@ -5,6 +5,7 @@ int main(void) {
   int x = 2;
   int y = 3;
   int x_y = x_to_y(x,y);
+  printf("Running version %s of mathlib\n", mathlib_version);
   printf("%d^%d = %d\n", x, y, x_y);
   return 0;
 }
