64ca192a8507da411cd353100ee9d92293fe543e
Joseph Bell <jbell@numerex.com>
2012-03-23 15:59:11 -0500

    Master updates

--- calculator.c
+++ calculator.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 
 void func_x(void);
+void func_y(void); // Added by master
+void func_z(void); // Added by master
 
 int main(void) {
   int x = 2;
@@ -14,10 +16,20 @@ int main(void) {
   int x_y = x_to_y(x,y);
   printf("Running version %s of mathlib\n", mathlib_version);
   func_x();
+  func_y();
+  func_z();
   printf("%d^%d = %d\n", x, y, x_y);
   return 0;
 }
 
 void func_x(void) {
   printf("Now you're playing with Nintendo power.\n");
+}
+
+void func_y(void) {
+  printf("Brought to you by, the master...\n");
+}
+
+void func_z(void) {
+  printf("and no one else!");
 }
