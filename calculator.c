be1215dd92174b52f519f5a9b109c390bc96de05
Joseph Bell <jbell@numerex.com>
2012-03-23 15:12:04 -0500

    Nintendo power

--- calculator.c
+++ calculator.c
@@ -1,11 +1,18 @@
 #include "mathlib.h"
 #include <stdio.h>
 
+void func_x(void);
+
 int main(void) {
   int x = 2;
   int y = 3;
   int x_y = x_to_y(x,y);
   printf("Running version %s of mathlib\n", mathlib_version);
+  func_x();
   printf("%d^%d = %d\n", x, y, x_y);
   return 0;
+}
+
+void func_x(void) {
+  printf("Now you're playing with Nintendo power.\n");
 }
